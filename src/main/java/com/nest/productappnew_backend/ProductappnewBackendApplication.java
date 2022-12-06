e36f22f63cb0da21ff0532d48792e446488eb94f
hami-r <mynameissaitama54@gmail.com>
2022-12-06 15:25:18 +0530

    initial commit

---
+++ src/main/java/com/nest/productappnew_backend/ProductappnewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.productappnew_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductappnewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductappnewBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nest/productappnew_backend/ProductappnewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.productappnew_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductappnewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
